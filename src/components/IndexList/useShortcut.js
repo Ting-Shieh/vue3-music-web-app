763ce9a2d8e90c32bcb8d1b8d8e39588ebf47b1b
Ting-Shieh <53999282+Ting-Shieh@users.noreply.github.com>
2022-09-25 11:08:47 +0800

    完善useShortcut.js。執行右側所以滾動條。

--- src/components/IndexList/useShortcut.js
+++ src/components/IndexList/useShortcut.js
@@ -1,7 +1,9 @@
 import { computed, ref } from 'vue'
 
 export default function useShortcut (props, groupRef) {
+  const ANCHOR_HEIGHT = 18
   const scrollRef = ref(null)
+  const touch = {}
   const shortcutList = computed(() => {
     return props.data.map(group => {
       return group.title
@@ -9,13 +11,31 @@ export default function useShortcut (props, groupRef) {
   })
 
   function onShortcutTouchStart (e) {
+    console.log('onShortcutTouchStart e', e)
     const anchorIndex = parseInt(e.target.dataset.index)
-    const targetEl = groupRef.value.children[anchorIndex]
-    const scroll = scrollRef.value.scroll
-    scroll.scrollToElement(targetEl, 0)
+    touch.anchorIndex = anchorIndex
+    touch.y1 = e.touches[0].pageY
+    scrollTo(anchorIndex)
   }
   function onShortcutTouchMove (e) {
-    console.log('onShortcutTouchMove e', e.target)
+    // console.log('onShortcutTouchMove e', e.target)
+    touch.y2 = e.touches[0].pageY
+    const delta = (touch.y2 - touch.y1) / ANCHOR_HEIGHT | 0 // 正數向下取整
+    const anchorIndex = touch.anchorIndex + delta
+    //
+    scrollTo(anchorIndex)
+  }
+  function scrollTo (index) {
+    // console.log('index:', index)
+    // 點擊外框滑動會出現NaN，視為無效拖動
+    if (isNaN(index)) {
+      return
+    }
+    // 控制 index 區間限制
+    index = Math.max(0, Math.min(shortcutList.value.length - 1, index))
+    const targetEl = groupRef.value.children[index]
+    const scroll = scrollRef.value.scroll
+    scroll.scrollToElement(targetEl, 0)
   }
   return {
     shortcutList,
