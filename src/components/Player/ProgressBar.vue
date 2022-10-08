c0c88d8c8d5b6097feddf15f433240e32395e164
Ting-Shieh <53999282+Ting-Shieh@users.noreply.github.com>
2022-10-08 22:44:43 +0800

    開發進度條功能相關邏輯開發-按鈕拖曳影響進度變更，計算方式還得利理解。

--- src/components/Player/ProgressBar.vue
+++ src/components/Player/ProgressBar.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="progress-bar">
+    <!-- 整個bar -->
     <div class="bar-inner">
-      <div class="progress" :style="progressStyle"></div>
-      <div class="progress-btn-wrapper" :style="btnStyle">
+      <!-- 黃色進度 -->
+      <div class="progress" :style="progressStyle" ref="progressRef"></div>
+      <!-- 拖動按鈕 -->
+      <div
+      class="progress-btn-wrapper "
+      :style="btnStyle"
+      @touchstart.prevent="onTouchStart"
+      @touchmove.prevent="onTouchMove"
+      @touchend.prevent="onTouchEnd"
+      >
         <div class="progress-btn"></div>
       </div>
     </div>
@@ -12,6 +21,7 @@
 const progressBtnWidth = 16
 export default {
   name: 'ProgressBar',
+  emits: ['progress-changing', 'progress-changed'],
   props: {
     progress: {
       type: Number,
@@ -39,9 +49,38 @@ export default {
   watch: {
     // 組件渲染後
     progress (newProgress) {
+      // 整個進度條寬度
       const barWidth = this.$el.clientWidth - progressBtnWidth
       this.offset = barWidth * newProgress
     }
+  },
+  methods: {
+    onTouchStart (e) {
+      // 起點位置 & 黃色進度條寬度
+      this.touch.x1 = e.touches[0].pageX
+      this.touch.beginWidth = this.$refs.progressRef.clientWidth
+    },
+    onTouchMove (e) {
+      // 相對初始位置偏移多少
+      const delta = e.touches[0].pageX - this.touch.x1
+      const tempWidth = this.touch.beginWidth + delta
+      const barWidth = this.$el.clientWidth - progressBtnWidth
+      // 限定在0~1
+      const progress = Math.min(1, Math.max((tempWidth / barWidth), 0))
+      this.offset = barWidth * progress
+      // 拖移btn時，同時影響歌曲
+      this.$emit('progress-changing', progress)
+    },
+    onTouchEnd () {
+      const barWidth = this.$el.clientWidth - progressBtnWidth
+      // 左側進度條寬度
+      const progress = this.$refs.progressRef.clientWidth / barWidth
+      // 拖移btn結束時，同時影響歌曲
+      this.$emit('progress-changed', progress)
+    }
+  },
+  created () {
+    this.touch = {}
   }
 }
 </script>
