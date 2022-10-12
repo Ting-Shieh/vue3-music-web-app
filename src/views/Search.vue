9c09e1bfeb1e59fd0dca256c7520adf88d0efdb2
Ting-Shieh <53999282+Ting-Shieh@users.noreply.github.com>
2022-10-12 21:00:00 +0800

    搜索組件SearchInput.vue Dom刻劃與初始化。

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="home">
+  <div class="search">
+    <div class="search-input-wrapper"></div>
   </div>
 </template>
 
 <script setup>
 
 </script>
+<style lang="scss" scoped>
+
+</style>
