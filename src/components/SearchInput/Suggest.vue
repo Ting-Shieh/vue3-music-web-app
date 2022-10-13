89b64ff1540265a7065aefcb059a7500215e518d
Ting-Shieh <53999282+Ting-Shieh@users.noreply.github.com>
2022-10-13 12:43:02 +0800

    搜尋頁面 Suggest.vue 組件 Dom刻畫與初始化。

---
+++ src/components/SearchInput/Suggest.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="suggest">
+    <ul class="suggest-list">
+      <li class="suggest-item" v-if="singer">
+        <div class="icon">
+          <i class="icon-mine"></i>
+        </div>
+        <div class="name">
+          <p class="text">{{song.name}}</p>
+        </div>
+      </li>
+      <li
+        class="suggest-item"
+        v-for="song in songs"
+        :key="song.id"
+      >
+        <div class="icon">
+          <i class="icon-music"></i>
+        </div>
+        <div class="name">
+          <p class="text">
+            {{song.singer}}-{{song.name}}
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script setup></script>
+<style lang="scss" scoped>
+.suggest {
+    height: 100%;
+    overflow: hidden;
+    .suggest-list {
+      padding: 0 30px;
+      .suggest-item {
+        display: flex;
+        align-items: center;
+        padding-bottom: 20px;
+        .icon {
+          flex: 0 0 30px;
+          width: 30px;
+          [class^="icon-"] {
+            font-size: 14px;
+            color: $color-text-d;
+          }
+        }
+        .name {
+          flex: 1;
+          font-size: $font-size-medium;
+          color: $color-text-d;
+          overflow: hidden;
+          .text {
+            @include no-wrap();
+          }
+        }
+      }
+    }
+  }
+</style>
