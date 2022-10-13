2a999485411dc0dd252910911a8d74d4fe1dc506
Ting-Shieh <53999282+Ting-Shieh@users.noreply.github.com>
2022-10-13 23:12:59 +0800

    新增添加歌曲到列表AddSong/index.vue Dom刻畫與初始化結構。

---
+++ src/components/AddSong/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <teleport to="body">
+    <transition name="slide">
+      <div class="add-song" v-show="visible">
+        <div class="header">
+          <h1 class="title">添加歌曲到列表</h1>
+          <div class="close">
+            <i class="icon-close"></i>
+          </div>
+        </div>
+        <div class="search-input-wrapper">
+          <search-input v-model="query" placeholder="搜索歌曲"></search-input>
+        </div>
+        <div class="search-result" v-show="query">
+          <suggest :show-singer="false" :query="query"></suggest>
+        </div>
+      </div>
+    </transition>
+  </teleport>
+</template>
+<script setup>
+import SearchInput from '@/component/SearchInput'
+import Suggest from '@/component/SearchInput/Suggest'
+import { ref } from 'vue'
+
+// data
+const query = ref('')
+const visible = ref(false)
+</script>
+<style lang="scss" scoped>
+.add-song {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  z-index: 300;
+  background: $color-background;
+  .header {
+    position: relative;
+    height: 44px;
+    text-align: center;
+    .title {
+      line-height: 44px;
+      font-size: $font-size-large;
+      color: $color-text;
+    }
+    .close {
+      position: absolute;
+      top: 0;
+      right: 8px;
+      .icon-close {
+        display: block;
+        padding: 12px;
+        font-size: 20px;
+        color: $color-theme;
+      }
+    }
+  }
+  .search-input-wrapper {
+    margin: 20px
+  }
+  .search-result {
+    position: fixed;
+    top: 124px;
+    bottom: 0;
+    width: 100%;
+  }
+}
+</style>
