dcbcdf42df25ffd5925b8979d5e661439672c1d5
Ting-Shieh <53999282+Ting-Shieh@users.noreply.github.com>
2022-10-14 12:57:15 +0800

    新增消息組件 components/Base/Meesage/index.vue Dom刻劃與初始化。

---
+++ src/components/Base/Message/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <teleport to="body">
+    <transition name="slide-down">
+      <div class="message" v-show="visible">
+        <slot></slot>
+      </div>
+    </transition>
+  </teleport>
+
+</template>
+<script>
+export default {
+  name: 'Message',
+  data () {
+    return {
+      visible: false
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.message{
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 400;
+  background: $color-dialog-background;
+  // 從上到下
+  &.slide-down-enter-active, &.slide-down-leave-active {
+    transition: all 0.3s
+  }
+
+  &.slide-down-enter-from, &.slide-down-leave-to {
+    transform: translate3d(0, -100%, 0)
+  }
+}
+</style>
